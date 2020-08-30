af3d5c6bf2bafec0ac1b2b9d1cc6711668653370
jaysunxiao <jaysunxiao@gmail.com>
2020-08-30 23:25:43 +0800

    C# base

--- GameFramework/Program.cs
+++ GameFramework/Program.cs
@@ -7,6 +7,12 @@ namespace GameFramework
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Gf!");
+            var bytes = Utility.Converter.GetBytes("Hello");
+            foreach (var b in bytes)
+            {
+                Console.WriteLine(b);
+            }
+            Console.WriteLine(Utility.Text.Format("Hello {0} {1}", "aa", "bb"));
         }
     }
 }
